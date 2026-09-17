3ea6de2d11ded65474525e31e523cda852863ab2
Code-Appathy <wood28st@gmail.com>
2026-09-18 07:33:12 +0900

    Add src/preload.js

---
+++ src/preload.js
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('obsidianApp', {
+  chooseVault: () => ipcRenderer.invoke('vault:choose'),
+  vaultStatus: () => ipcRenderer.invoke('vault:status'),
+  listNotes: () => ipcRenderer.invoke('notes:list'),
+  readNote: (filePath) => ipcRenderer.invoke('notes:read', filePath),
+  createNote: (title) => ipcRenderer.invoke('notes:create', title),
+  saveNote: (filePath, content) => ipcRenderer.invoke('notes:save', filePath, content),
+  deleteNote: (filePath) => ipcRenderer.invoke('notes:delete', filePath)
+});
+
